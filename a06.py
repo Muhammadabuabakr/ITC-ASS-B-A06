86ccfddb1b7be05289d8db17b56ed9b84d57d8f3
Muhammadabuabakr <31399732+Muhammadabuabakr@users.noreply.github.com>
2018-03-26 23:12:48 +0500

    assignment completed.

---
+++ a06.py
@@ -0,0 +1,46 @@
+## IMPORTS GO HERE
+
+## END OF IMPORTS
+
+
+### YOUR CODE FOR cumulative_marks() FUNCTION GOES HERE ###
+def cumulative_marks(string):
+    lst =[]
+    lst1 = []
+    d_marks = {}
+    marks = 0
+
+    if string == None:
+        return None
+
+    for i in string:
+        roll_no = i[0]
+        batch = i[0][1:3]
+        p = i[0][0].upper() + str('-')
+        roll_no = batch + p + str(roll_no[3:])
+        names = i[1]
+        if len(i) >= 3:
+            for j in i[2:]:
+                if j == None or type(j) == str:
+                    marks+=0
+                else:
+                    marks += j
+                d_marks[roll_no] = marks
+            marks = 0
+        else:
+            d_marks[roll_no] = marks
+        lst = roll_no,names,d_marks[roll_no]
+        lst1.append(lst)
+
+    return lst1
+#### End OF MARKER
+
+if __name__ == '__main__':
+    results = [
+        ('p101111', 'Muhammad Amin', 64, 78.5, 89, 25, 99),
+        ('p101112', 'Tehseen Khan', 14, 28.5, 83, 76),
+        ('p101113', 'Tauqeer Ali', 87, None, 1.6)
+    ]
+
+#    print cumulative_marks(results)
+    # output: [('10P-1111', 'Muhammad Amin', 355.5), ('10P-1112', 'Tehseen Khan', 201.5), ('10P-1113', 'Tauqeer Ali', 88.6)]
